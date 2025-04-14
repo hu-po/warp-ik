33123278e36a00b408a958e649dc823d38536d53
hu.po <hugo.p.cmu@gmail.com>
2025-04-14 09:34:22 -0500

    asd

--- scripts/docker.test.sh
+++ scripts/docker.test.sh
@@ -6,6 +6,7 @@ docker run --gpus all -it --rm --user="root" \
 -v $ROOT_DIR/.env:/root/warp-ik/.env \
 -v $ROOT_DIR/output:/root/warp-ik/output \
 warp-ik-$DOCKERFILE bash -c "
+uv run pip freeze && \
 uv run python /root/warp-ik/src/test.py && \
 uv run python /root/warp-ik/src/ai.py --test && \
 uv run python /root/warp-ik/src/warp/device_properties.py"
